e6b2166f160b9a5e79e2336e5fac045053921444
Skylowerr <gokceemirhan23@gmail.com>
2026-05-08 19:57:36 +0200

    Initial Commit

---
+++ AdvancedSwift/AdvancedSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedSwiftApp.swift
+//  AdvancedSwift
+//
+//  Created by Emirhan Gökçe on 8.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
