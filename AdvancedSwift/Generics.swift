257f73142f38f06d22cf47374f610283854b522c
Skylowerr <gokceemirhan23@gmail.com>
2026-05-13 23:21:39 +0200

    [Feature] Created Generics

---
+++ AdvancedSwift/Generics.swift
@@ -0,0 +1,18 @@
+//
+//  Generics.swift
+//  AdvancedSwift
+//
+//  Created by Emirhan Gökçe on 13.05.2026.
+//
+
+import SwiftUI
+
+struct Generics: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Generics()
+}
